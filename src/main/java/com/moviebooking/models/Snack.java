acc8c7fa31ca6cfce840720ce532f982c266a890
Mehmetalp Sancakçeken <mehmetalpsancakceken@gmail.com>
2025-12-17 16:19:09 +0300

    feat: #8 define Snack class fields and constructor.

---
+++ src/main/java/com/moviebooking/models/Snack.java
@@ -0,0 +1,10 @@
+package main.java.com.moviebooking.models;
+
+public class Snack {
+    private String name;
+    private double price;
+    public Snack(String name, double price){
+        this.name = name;
+        this.price = price;
+    }
+}
