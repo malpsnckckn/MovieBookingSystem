54747904ce210fb42ef924da32b302a7fb0b078c
Mehmetalp Sancakçeken <mehmetalpsancakceken@gmail.com>
2025-12-18 15:08:15 +0300

    #14 implement Movie3D subclass and extra glasses price.

--- src/main/java/com/moviebooking/models/Movie3D.java
+++ src/main/java/com/moviebooking/models/Movie3D.java
@@ -1,5 +1,12 @@
 package main.java.com.moviebooking.models;
 
-public class Movie3D {
-    
+public class Movie3D extends Movie{
+    private final double glasses_price = 25.0;
+    public Movie3D(String title, String genre, int durationMinute, double basePrice){
+        super(title, genre, durationMinute, basePrice);
+    }
+    @Override
+    public double getTicketPrice(){
+        return basePrice + glasses_price;
+    }
 }
