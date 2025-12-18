208126a5db01e965e9e31ca43a5cdbd441b64b0c
Mehmetalp Sancakçeken <mehmetalpsancakceken@gmail.com>
2025-12-18 14:40:55 +0300

    #12 create Movie abstract class.

---
+++ src/main/java/com/moviebooking/models/Movie.java
@@ -0,0 +1,20 @@
+package main.java.com.moviebooking.models;
+
+public abstract class Movie {
+    protected String title;
+    protected String genre;
+    protected int durationMinutes;
+    protected double basePrice;    
+
+    public Movie(String title, String genre, int durationMinutes, double basePrice){
+        this.title = title;
+        this.genre = genre;
+        this.durationMinutes = durationMinutes;
+        this.basePrice = basePrice;
+    }
+    public abstract double getTicketPrice();
+    public String getTittle(){
+        return title;
+    }
+}
+
