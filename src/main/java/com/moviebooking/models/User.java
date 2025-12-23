83b44c8b37395b23f3de439155b857808c6a87ae
Mehmetalp Sancakçeken <mehmetalpsancakceken@gmail.com>
2025-12-23 14:21:00 +0300

    #21 added User model with reservaiton history support.

---
+++ src/main/java/com/moviebooking/models/User.java
@@ -0,0 +1,32 @@
+package main.java.com.moviebooking.models;
+import java.util.ArrayList;
+import java.util.List;;
+public class User {
+    private String username;
+    private String password;
+    private List<Reservation> reservationHistory;
+
+    public User( String username, String password){
+        this.username = username;
+        this.password = password;
+        this.reservationHistory = new ArrayList<>();
+    }
+    public String getUsername(){
+        return username;
+    }
+    public boolean checkPassword(String password){
+        return this.password.equals(password);
+    }
+    public void addReservation(Reservation reservation){
+        reservationHistory.add(reservation);
+    }
+    public List<Reservation> getReservationHistory(){
+        return reservationHistory;
+    }
+    @Override
+    public String toString(){
+        return "User: " + username +
+               " | Reservations: " + reservationHistory.size();
+    }
+    
+}
