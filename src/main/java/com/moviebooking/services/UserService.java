3c68b97c576d73f66b3d94062bbfbca04fca69a7
Mehmetalp Sancakçeken <mehmetalpsancakceken@gmail.com>
2025-12-26 16:36:06 +0300

    #26 Create UserService base structure.

---
+++ src/main/java/com/moviebooking/services/UserService.java
@@ -0,0 +1,15 @@
+package com.moviebooking.services;
+
+import com.moviebooking.models.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserService {
+
+    private List<User> users;
+
+    public UserService() {
+        users = new ArrayList<>();
+    }
+}
